9a97c6ca024bb20842c7eaaae58789806dbe8b40
iksadNorth <iksadnorth@gmail.com>
2022-09-07 15:11:37 +0900

    #2 - 실험들을 위한 기본적인 구조 생성
    간단한 뷰 생성.
    1. 로그인 페이지 생성.
    2. 인가를 위한 관리자 페이지 생성.
    3. 인가를 위한 게시판 글쓰기 페이지 생성.
    4. 홈페이지 생성.

--- frontend/src/route/index.js
+++ frontend/src/route/index.js
@@ -6,6 +6,21 @@ const routes = [
     name: 'Home',
     component: () => import('../views/view_Home.vue'), 
   },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/view_Login.vue'), 
+  },
+  {
+    path: '/memo',
+    name: 'Memo',
+    component: () => import('../views/view_Memo.vue'), 
+  },
+  {
+    path: '/dash',
+    name: 'DashBoard',
+    component: () => import('../views/view_DashBoard.vue'), 
+  },
 ];
 
 const router = createRouter({
---
+++ frontend/src/views/view_DashBoard.vue
@@ -0,0 +1,60 @@
+<template>
+    <v-container class="w-50">
+        <v-row class="my-5 text-center">
+            <h2>관리자 페이지</h2>
+        </v-row>
+
+        <v-row class="my-5">
+            <v-textarea
+            v-model="text"
+            label="입력창"
+            auto-grow
+            ></v-textarea>
+        </v-row>
+        
+        <v-row class="my-5">
+            <v-btn
+            outlined
+            @click="clickWrite()"
+            >쓰기</v-btn>
+        </v-row>
+
+        <v-row class="justify-center">
+            <v-table>
+                <thead>
+                    <tr>
+                        <th class="text-left">
+                        text
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        v-for="item in items"
+                        :key="item"
+                    >
+                        <td>{{ item }}</td>
+                    </tr>
+                </tbody>
+            </v-table>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            items: [],
+
+            text: undefined,
+        }
+    },
+    methods: {
+        clickWrite() {
+            // 쓰기 버튼 클릭
+            this.items.push(this.text);
+        }
+    },
+}
+</script>
